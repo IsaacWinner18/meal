0b05061d3396b4f81acf6cbfdb66e9751515b4a8
IsaacWinner18 <winsamlab222@gmail.com>
2025-02-07 21:39:48 +0100

    BEN!!!, he came and ruin my code.

--- frontend/app/page.jsx
+++ frontend/app/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useSearchParams } from "next/navigation";
+// import { useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
 import { Progress } from "@/components/ui/progress";
 import { Button } from "@/components/ui/button";
@@ -8,7 +8,7 @@ import { motion } from "framer-motion";
 // import Image from "next/image";
 import Footer from "@/components/Footer";
 import Videos from "@/components/videos";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 // import {
 //   Card,
@@ -21,7 +21,7 @@ import { useRouter } from "next/router";
 // import Link from "next/link";
 
 export default function Home() {
-  const searchParams = useSearchParams();
+  // const searchParams = useSearchParams();
   const [balance, setBalance] = useState(0);
   const [progress, setProgress] = useState(0);
   const [canClaim, setCanClaim] = useState(true);
@@ -32,7 +32,7 @@ export default function Home() {
   // const [videos, setVideos] = useState([]);
   const [lastClaimed, setLastClaimed] = useState(null);
   const [claimedVideos, setClaimedVideos] = useState([]);
-  const router = useRouter();
+  // const router = useRouter();
 
   console.log(
     "This is the videoId that needs to be stored frontend",
@@ -99,17 +99,8 @@ export default function Home() {
     };
 
     loadTelegramSDK();
-  }, [router.pathname]);
+  }, []);
 
-  useEffect(() => {
-    // Extract the "start" parameter (Telegram referral code)
-    const startParam = searchParams.get("start");
-
-    if (startParam) {
-      setReferredBy(startParam);
-      console.log("Extracted Referral Code:", startParam);
-    }
-  }, [searchParams]);
 
   // useEffect(() => {
   //   // I uncomment when testing locally
