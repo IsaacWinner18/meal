8981b20590d362c7a2148894c614e9a077a87966
IsaacWinner18 <winsamlab222@gmail.com>
2025-02-15 15:13:36 +0100

    ahh, on referral now

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Suspense } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +23,7 @@ export default function RootLayout({ children }) {
         <body
           className={`${geistSans.variable} ${geistMono.variable} bg-gradient-to-b from-neutral-900 to-black antialiased`}
         >
-          <Suspense>{children}</Suspense>
+          {children}
         </body>
       </html>
     </>
