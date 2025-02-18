a7978a36bd3d88508370c67657dc0875b8361b94
IsaacWinner18 <winsamlab222@gmail.com>
2025-02-18 19:53:00 +0100

    oya push am push am

--- frontend/app/earn/page.jsx
+++ frontend/app/earn/page.jsx
@@ -1,41 +1,41 @@
-// "use client";
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
-import { Button } from "@/components/ui/button"
-import { Gift, PlayCircle, Share2 } from "lucide-react"
-import Image from "next/image";
-import Link from 'next/link';
+"use client";
+// import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+// import { Button } from "@/components/ui/button"
+// import { Gift, PlayCircle, Share2 } from "lucide-react"
+// import Image from "next/image";
+// import Link from 'next/link';
 import Videos from "@/components/videos";
 
 
 export default function Earn() {
 
-  const handleClaim = async (videoId) => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/dashboard`,
-        {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
+  // const handleClaim = async (videoId) => {
+  //   try {
+  //     const response = await fetch(
+  //       `${process.env.NEXT_PUBLIC_API_URL}/dashboard`,
+  //       {
+  //         method: "PATCH",
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
 
-          body: JSON.stringify({
-            userId: peopleData.userId,
-            videoId: videoId,
-          }),
-        }
-      );
+  //         body: JSON.stringify({
+  //           userId: peopleData.userId,
+  //           videoId: videoId,
+  //         }),
+  //       }
+  //     );
 
-      if (!response.ok) {
-        throw new Error("failed to update mlcoin");
-      }
+  //     if (!response.ok) {
+  //       throw new Error("failed to update mlcoin");
+  //     }
 
-      const data = await response.json();
-      alert("Successfully claimed")
-    } catch (error) {
-      console.log(`The adeola123 error ${error}`);
-    }
-  }
+  //     const data = await response.json();
+  //     alert("Successfully claimed")
+  //   } catch (error) {
+  //     console.log(`The adeola123 error ${error}`);
+  //   }
+  // }
   return (
     (<div className="max-w-md mx-auto p-4 min-h-screen flex flex-col">
       <div className="flex items-center justify-between mb-3">
