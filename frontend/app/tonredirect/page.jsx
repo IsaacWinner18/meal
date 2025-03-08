5a228604cc703d2f451137e88748e7211f711e64
IsaacWinner18 <winsamlab222@gmail.com>
2025-03-08 23:16:29 +0100

    ad

--- frontend/app/tonredirect/page.jsx
+++ frontend/app/tonredirect/page.jsx
@@ -11,7 +11,7 @@ export default function TonRedirect() {
     const amount = param.get('amount')
     const userId = param.get('userId')
 
-    if (amount) {
+    if (amount && userId) {
         // window.location.href = `ton://transfer/UQDE7SRvOuBokRenwICsFw3XOUWcx1SYiHKu_ssrewOQfEtW?amount=${amount}&text=${userData?.userId}`;
 
         window.location.href = `ton://transfer/0QDMoTcRiP_ciARdO2CFDvAlZ_-V70wyVl4w2na66b_oHjzV?amount=${amount}&text=${userId}`;
