0bbba3c7bf9ce3dd491eee778a60d89197ba5ee8
IsaacWinner18 <winsamlab222@gmail.com>
2025-03-14 01:27:46 +0100

    fuck it

--- frontend/app/veiw/page.jsx
+++ frontend/app/veiw/page.jsx
@@ -205,9 +205,9 @@ export default function View({ userData, refCode }) {
     }
   }, [lastClaimed]);
 
-  const clearLocal = () => {
-    localStorage.clear();
-  }
+  // const clearLocal = () => {
+  //   localStorage.clear();
+  // }
 
   return (
     <div className="max-w-md mx-auto min-h-screen flex flex-col">
@@ -241,7 +241,9 @@ export default function View({ userData, refCode }) {
         </div>
         <div className="bg-neutral-900 rounded-full text-center my-4 shadow-inner shadow-black inset-">
           <span className="text-white text-sm font-mono">
-            EARNING RATE +1000.00 MLC/day <Button onClick={clearLocal()}>clear local storage </Button> {" "}
+            EARNING RATE +1000.00 MLC/day 
+            {/* <Button onClick={clearLocal()}>clear local storage </Button> */}
+             {" "}
           </span>
         </div>
 
