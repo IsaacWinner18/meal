a0e393613762ba8448b88790820f0d3906831775
IsaacWinner18 <winsamlab222@gmail.com>
2025-03-22 01:17:42 +0100

    Disable polling for Telegram bot and enable webhook; comment out unused user data effects in frontend

--- backend/index.js
+++ backend/index.js
@@ -35,20 +35,20 @@ app.listen(5000, async() => {
 app.use("/uploads", express.static(path.join(__dirname, "public/uploads")))
  
 const token = process.env.TELEGRAM_TOKEN
-const bot = new telegramBot(token, {polling: true});
+const bot = new telegramBot(token, {polling: false});
 const webhookUrl = `${process.env.NEXT_PUBLIC_API_URL}/bot` + token;
-// console.log(webhookUrl)
-// bot.setWebHook(webhookUrl)
+console.log(webhookUrl)
+bot.setWebHook(webhookUrl)
 
-// app.post(`/bot${token}`, (req, res) => {
-//     try {
-//         bot.processUpdate(req.body)
-//         res.sendStatus(200);
-//     } catch (err) {
-//         console.log("Error processing update:", err)
-//         res.sendStatus(500)
-//     }
-// });
+app.post(`/bot${token}`, (req, res) => {
+    try {
+        bot.processUpdate(req.body)
+        res.sendStatus(200);
+    } catch (err) {
+        console.log("Error processing update:", err)
+        res.sendStatus(500)
+    }
+});
 
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
